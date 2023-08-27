7db675cc5ae2ef33814524d830504c8e97c02ad6
Deba Dey <debadey886@gmail.com>
2023-08-28 00:36:21 +0530

    Add files via upload

---
+++ burger/public/data.js
@@ -0,0 +1,44 @@
+const burgerData = [
+    {
+      name: "Smokey Maple Bacon Burger",
+      ingredients: "Juicy beef patty topped with crispy bacon, melted smoked Gouda, and a drizzle of rich maple aioli.",
+      price: 6,
+      photoName: "burgers/Burger1.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Spicy Guacamole Fiesta Burger",
+      ingredients: "A fiery blend of jalapeño jack cheese, zesty guacamole, and chipotle mayo piled high on a seasoned beef patty.",
+      price: 10,
+      photoName: "burgers/Burger2.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Mediterranean Lamb Feta Burger",
+      ingredients: "Ground lamb infused with Mediterranean spices, crumbled feta, roasted red peppers, and a refreshing tzatziki sauce.",
+      price: 12,
+      photoName: "burgers/Burger3.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Asian Fusion Teriyaki Burger",
+      ingredients: "Teriyaki-glazed beef patty accompanied by grilled pineapple, pickled daikon radish, and a ginger-sesame mayo.",
+      price: 12,
+      photoName: "burgers/Burger4.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Hawaiian Teriyaki Burger",
+      ingredients: "Beef patty glazed with teriyaki sauce, topped with grilled pineapple, Swiss cheese, and lettuce.",
+      price: 15,
+      photoName: "burgers/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Greek Gyro Burger",
+      ingredients: "Ground lamb patty with tzatziki sauce, feta cheese, red onion, cucumber, and lettuce.",
+      price: 18,
+      photoName: "burgers/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
